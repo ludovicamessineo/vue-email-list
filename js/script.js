37d090eecf2da90a5f65971e651cd194c72e0e20
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-05-16 14:53:47 +0200

    exercise completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+const app = new Vue (
+    {
+        el: "#root",
+        data: {
+            emailArray: []
+        },
+
+        created() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail',)
+                .then((resp) => {
+                    const randomEmail = resp.data.response
+                    this.emailArray.push(randomEmail)
+                })
+            }
+        }
+})
+
+
